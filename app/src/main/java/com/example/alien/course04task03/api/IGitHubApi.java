0ca2820f0577b97c41dbde7a745c9971356bedbd
alien190 <alien1900@bk.ru>
2018-08-19 20:28:33 +0700

    create User class from JSON

--- app/src/main/java/com/example/alien/course04task03/api/IGitHubApi.java
+++ app/src/main/java/com/example/alien/course04task03/api/IGitHubApi.java
@@ -2,7 +2,6 @@ package com.example.alien.course04task03.api;
 
 
 import io.reactivex.Single;
-import com.example.alien.course04task03.model.User;
 
 public interface IGitHubApi {
     Single<User> getUser(String token);
