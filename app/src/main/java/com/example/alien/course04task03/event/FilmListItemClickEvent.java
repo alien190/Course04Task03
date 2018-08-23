e99565a1ad6c76b02ce7ffef58cd4ac6b332e5a9
alien190 <alien1900@bk.ru>
2018-08-23 13:43:47 +0700

    delete unused event

--- app/src/main/java/com/example/alien/course04task03/event/FilmListItemClickEvent.java
+++
@@ -1,9 +0,0 @@
-package com.example.alien.course04task03.event;
-
-public class FilmListItemClickEvent {
-    public long id;
-
-    public FilmListItemClickEvent(long id) {
-        this.id = id;
-    }
-}
